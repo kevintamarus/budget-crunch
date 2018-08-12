d023d378c21d0ab1788215fae24083375a2903eb
Kevin Tamarus <kevin@endpointclosing.com>
2018-08-11 23:27:42 -0700

    some folders and components added

---
+++ src/common/Text.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text as RNText } from 'react-native';
+
+const Text = props => {
+  return <Text>{props.children}</Text>;
+};
+
+export default Text;
---
+++ src/pages/Main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export default class Main extends React.Component {
+  render() {
+    return <View />;
+  }
+}
