ee4421267fc7e8584c8c67421efaf72ee757c276
Kevin Tamarus <kevintamarus@gmail.com>
2018-09-15 11:34:51 -0700

    react-native-elements vments vector icons library added, modified card a little

--- src/common/AmountCard.js
+++ src/common/AmountCard.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Icon } from 'react-native-elements';
+
+import Text from '../common/Text';
 
 const AmountCard = props => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Card</Text>
+      <Icon name="close" type="material-community" color="#000000" />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    justifyContent: 'space-between'
+  }
+});
 
 export default AmountCard;
--- src/common/Text.js
+++ src/common/Text.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { StyleSheet, Text as RNText } from 'react-native';
 
 const Text = props => {
-  const styles = StyleSheet.create({
-    text: {
-      fontSize: 25,
-      ...props.style
-    }
-  });
-  return <RNText style={styles.text}>{props.children}</RNText>;
+  return <RNText style={[styles.text, props.style]}>{props.children}</RNText>;
 };
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 25
+  }
+});
 
 export default Text;
