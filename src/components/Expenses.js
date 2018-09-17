01506821af524cdaf480a736fdfd289fbaaee832
Kevin Tamarus <kevintamarus@gmail.com>
2018-09-16 18:18:10 -0700

    add button implemented, no logic

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
+import { Icon } from 'react-native-elements';
 import Button from 'common/Button.js';
 import AmountCard from 'common/AmountCard';
 import Text from 'common/Text';
@@ -8,7 +9,12 @@ class Expenses extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>Expenses</Text>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Expenses</Text>
+          <View style={styles.addButton}>
+            <Icon name="plus" type="material-community" />
+          </View>
+        </View>
         <ScrollView contentContainerStyle={styles.container}>
           <AmountCard />
           <AmountCard />
@@ -23,11 +29,24 @@ class Expenses extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    minWidth: '100%',
-    alignItems: 'center'
+    minWidth: '100%'
+  },
+  titleContainer: {
+    flex: 0,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
   title: {
     textDecorationLine: 'underline'
+  },
+  addButton: {
+    width: 30,
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: '#a3a3a3',
+    borderWidth: 1,
+    borderRadius: 100
   },
   scrollContainer: {
     minWidth: '100%'
